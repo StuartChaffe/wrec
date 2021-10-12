ff36f92bf92841680f791da5930ba0b77229a8d5
Stuart Chaffe <stu@stuartchaffe.co.uk>
2021-10-12 13:04:59 +0100

    Add strapline to WP

--- wp-content/themes/wrec-theme/partials/global-header.php
+++ wp-content/themes/wrec-theme/partials/global-header.php
@@ -3,7 +3,7 @@
 		<div class="global-header--logo"><a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/src/images/logo.svg" alt="Whitehead Ross Logo" /></a></div>
 
 		<div class="global-header--content">
-			<p>A leading provider of government funded training and social welfare programmes</p>
+			<?php the_field('strapline_text', 'options'); ?>
 			<nav class="global-header--nav global-header--nav-desktop"><?php wp_nav_menu( array('theme_location' => 'primary') ); ?></nav>		</div>
 	</div>
 	<nav class="global-header--nav global-header--nav-mobile"><?php wp_nav_menu( array('theme_location' => 'primary') ); ?></nav>
