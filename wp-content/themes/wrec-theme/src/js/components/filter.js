6cd418ad7e57fd821fef7c273ffe6edb2589f51c
Marc Jenkins <marc@16by9.net>
2021-10-27 16:25:25 +0100

    Stop mix it up erroring on none filter pages

--- wp-content/themes/wrec-theme/src/js/components/filter.js
+++ wp-content/themes/wrec-theme/src/js/components/filter.js
@@ -1,51 +1,59 @@
-var containerEl = document.querySelector('.filter');
-var inputEl = document.querySelector('#filter-input');
-var initialFilter = 'all';
-var hash = window.location.hash.replace(/^#/g, '');
+(function () {
 
-if (hash) {
-    initialFilter = '.' + hash;
-}
+    var containerEl = document.querySelector('.filter');
+    var inputEl = document.querySelector('#filter-input');
+    var initialFilter = 'all';
+    var hash = window.location.hash.replace(/^#/g, '');
 
-var mixer = mixitup(containerEl, {
-    animation: {
-        duration: 300
-    },
-    multifilter: {
-        enable: true
-    },
-    pagination: {
-        limit: 9
-    },
-    load: {
-        filter: initialFilter
+    if (!containerEl) {
+        return false;
     }
-});
 
-// Search input
-inputEl.addEventListener('keydown', (event) => {
+    if (hash) {
+        initialFilter = '.' + hash;
+    }
 
-    setTimeout(function() {
-    
-        var inputText = inputEl.value.toLowerCase();
-        var collection = Array.from(containerEl.querySelectorAll('.mix'));
-
-        if (inputText && inputText.length) {
-            // console.log("You have pressed key: ", inputText);
-
-            var filtered = collection.filter(function(target) {
-                return target.getAttribute('data-title').toLowerCase().match(inputText);
-            });
-
-            mixer.filter(filtered);
-
-        } else {
-            mixer.filter(collection);
+    var mixer = mixitup(containerEl, {
+        animation: {
+            duration: 300
+        },
+        multifilter: {
+            enable: true
+        },
+        pagination: {
+            limit: 9
+        },
+        load: {
+            filter: initialFilter
         }
+    });
 
-    }, 250);
-    
-});
+    // Search input
+    inputEl.addEventListener('keydown', (event) => {
+
+        setTimeout(function() {
+        
+            var inputText = inputEl.value.toLowerCase();
+            var collection = Array.from(containerEl.querySelectorAll('.mix'));
+
+            if (inputText && inputText.length) {
+                // console.log("You have pressed key: ", inputText);
+
+                var filtered = collection.filter(function(target) {
+                    return target.getAttribute('data-title').toLowerCase().match(inputText);
+                });
+
+                mixer.filter(filtered);
+
+            } else {
+                mixer.filter(collection);
+            }
+
+        }, 250);
+        
+    });
+
+}());
 
 // Show filter
 jQuery(function ($) {
