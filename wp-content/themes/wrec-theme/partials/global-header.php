0089421fd9b29732909c30a3f50ee33191c7e03b
Stuart Chaffe <stu@stuartchaffe.co.uk>
2021-11-19 10:23:51 +0000

    Add logo to header

--- wp-content/themes/wrec-theme/partials/global-header.php
+++ wp-content/themes/wrec-theme/partials/global-header.php
@@ -1,6 +1,9 @@
 <header class="global-header">
 	<div class="global-header--inner">
-		<div class="global-header--logo"><a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/src/images/logo.png" alt="Whitehead Ross Logo" /></a></div>
+		<div class="global-header--logos">
+			<div class="global-header--logo"><a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/src/images/logo.png" alt="Whitehead Ross Logo" /></a></div>
+			<div class="global-header--logo-2"><img src="<?php echo get_template_directory_uri(); ?>/src/images/ESF-logo.png" alt="European Union Logo" /></div>
+		</div>
 
 		<div class="global-header--content">
 			<?php the_field('strapline_text', 'options'); ?>
