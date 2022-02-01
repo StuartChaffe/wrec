ca24f4849260fa364d666f01686529ccc8fcfd43
Stuart Chaffe <stu@stuartchaffe.co.uk>
2022-02-01 16:18:02 +0000

    Rewind master

--- wp-content/themes/wrec-theme/src/js/components/accordion.js
+++ wp-content/themes/wrec-theme/src/js/components/accordion.js
@@ -1,16 +1,16 @@
 // FAQ
-// jQuery(function ($) {
-//     $('.accordion .accordion__title').click(function() {
-//         $('.accordion .accordion__title').removeClass('active');
-//         $('.accordion .accordion__content').slideUp('normal');
-//         if($(this).next().is(':hidden') == true) {
-//             $(this).addClass('active');
-//             $(this).next().slideDown('normal');
-//         }
-//         return false;
-//     });
-//     $('.accordion .accordion__content').hide();
-//  })
+jQuery(function ($) {
+    $('.accordion .accordion__title').click(function() {
+        $('.accordion .accordion__title').removeClass('active');
+        $('.accordion .accordion__content').slideUp('normal');
+        if($(this).next().is(':hidden') == true) {
+            $(this).addClass('active');
+            $(this).next().slideDown('normal');
+        }
+        return false;
+    });
+    $('.accordion .accordion__content').hide();
+ })
 
 
  document.addEventListener("DOMContentLoaded", function() {
