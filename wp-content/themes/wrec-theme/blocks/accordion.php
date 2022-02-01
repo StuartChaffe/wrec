fc4b85b744559b61699ec631be3b2512c0eb72fb
Stuart Chaffe <stu@stuartchaffe.co.uk>
2022-02-01 16:20:55 +0000

    Add URL to accordin

--- wp-content/themes/wrec-theme/blocks/accordion.php
+++ wp-content/themes/wrec-theme/blocks/accordion.php
@@ -2,9 +2,8 @@
 /**
 * The template used for displaying an accordion.
 */
-// TITLE? $content = get_field( 'accordion_content');
 ?>
-<section>
+<!-- <section>
 	<?php if( have_rows('accordion') ): ?>
 		<ul class="accordion">
 		<?php while( have_rows('accordion') ): the_row();
@@ -20,4 +19,31 @@
 		<?php endwhile; ?>
 		</ul>
 	<?php endif; ?>
+</section> -->
+
+
+<?php
+/**
+* The template used for displaying an accordion.
+*/
+?>
+<section>
+	<?php if( have_rows('accordion') ): ?>
+	<div class="accordion">
+		<?php while( have_rows('accordion') ): the_row();
+			$title = get_sub_field('accordion_title', false, false);
+			$content = get_sub_field('accordion_content');
+		?>
+			<div class="accordion__item">
+				<button aria-expanded="false" class="accordion__handle" aria-controls="accordion-<?php echo sanitize_title($title); ?>" id="<?php echo sanitize_title($title); ?>">
+					<div class="accordion__title"><?php echo $title; ?> <svg class="icon icon--arrow"><use xlink:href="#arrow"></use></svg></div>
+				</button>
+				<div class="accordion__body" id="accordion-<?php echo sanitize_title($title); ?>" role="region" aria-labelledby="<?php echo sanitize_title($title); ?>">
+					<?php echo $content ?>
+				</div>
+			</div>
+		<?php endwhile; ?>
+	</div>
+	<?php endif; ?>
 </section>
+
